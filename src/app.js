ccee142469151cae6fd805b6126ef9bb3aac7dfe
Rafael Flores <rafafloresok@gmail.com>
2023-02-15 13:37:49 -0300

    add uuid. add response status codes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
-//Desarrollar un servidor express que, en su archivo app.js importe al archivo de ProductManager que actualmente tenemos.
 import express from "express";
-import __dirname from "./utils.js";
+import { __dirname } from "./utils.js";
 import ProductManager from "./ProductManager.js";
 
 const app = express();
@@ -11,24 +10,21 @@ app.use(express.urlencoded({ extended: true }));
 
 const pm = new ProductManager(`${__dirname}/files/products.json`);
 
-//El servidor debe contar con los siguientes endpoints:
-//ruta ‘/products’, la cual debe leer el archivo de productos y devolverlos dentro de un objeto. Agregar el soporte para recibir por query param el valor ?limit= el cual recibirá un límite de resultados.
-//Si no se recibe query de límite, se devolverán todos los productos
-//Si se recibe un límite, sólo devolver el número de productos solicitados
 app.get("/products", async (req, res) => {
   let limit = req.query.limit;
   let products = await pm.getProducts();
-  res.send({ products: products.slice(0, limit) });
+  res.setHeader("Content-Type", "application/json");
+  res.status(200).json({ products: products.slice(0, limit) });
 });
 
-//ruta ‘/products/:pid’, la cual debe recibir por req.params el pid (product Id), y devolver sólo el producto solicitado, en lugar de todos los productos.
 app.get("/products/:pid", async (req, res) => {
-  let id = parseInt(req.params.pid);
+  let id = req.params.pid;
   let product = await pm.getProductById(id);
+  res.setHeader("Content-Type", "application/json");
   if (product) {
-    res.send(product);
+    res.status(200).json(product);
   } else {
-    res.send({ error: "el producto no existe" });
+    res.status(400).json({ error: "el producto no existe" });
   }
 });
 
